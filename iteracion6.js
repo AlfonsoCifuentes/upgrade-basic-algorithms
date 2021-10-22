9a18b67ed9fe61d4e88abd17fa7b532376b6852b
Alfonso Cifuentes <alfonsocifuentes83@gmail.com>
2021-10-22 19:27:27 +0200

    Iteration#6

---
+++ iteracion6.js
@@ -0,0 +1,26 @@
+//Iteración #6: Bucles
+
+//1.1
+
+for (let i=0; i<10; i++){
+    console.log(i);
+}
+
+//1.2
+
+for (let i=0; i<10; i++){
+    if ((i % 2) === 0){
+        console.log (i);
+    }
+}
+
+//1.3
+
+for (let i=0; i<11; i++){
+    if (i<9) {
+        console.log ("Intentando Dormir")
+    }if (i === 10){
+        console.log ("Dormido!")
+        
+    }
+}
