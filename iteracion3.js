7e59f1063264601a9e60f654dcb8ad8a350f5aae
Alfonso Cifuentes <alfonsocifuentes83@gmail.com>
2021-10-22 15:24:29 +0200

    Iteration#3

---
+++ iteracion3.js
@@ -0,0 +1,24 @@
+//Iteracion #3: Operadores
+
+//1.1
+
+alert (10*5);
+
+//1.2
+
+alert (10/2);
+
+//1.3
+
+alert (15%9);
+
+//1.4
+
+const y = 10;
+const z = 5;
+
+var x = (y+z);
+
+//1.5
+
+var x = (y*z);
